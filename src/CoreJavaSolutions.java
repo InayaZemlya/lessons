90475f0b0e104b35cdda2cb69869d797f0b259ec
Vika_Inozemceva <inozemceva.vika@bk.ru>
2024-06-11 13:49:01 +0300

    The repository has been structured was structured for better perception

--- src/CoreJavaSolutions.java
+++ src/CoreJavaSolutions.java
@@ -1,27 +1,25 @@
 //Реализуйте метод, вычисляющий количество високосных лет с начала нашей эры (первого
-// года) до заданного года включительно. Вам надо проанализировать переданное
-// в метод значение и вернуть результат, не используя циклы. Придумайте и запишите
-// несложную формулу, использующую только арифметические операторы.
+// года) до заданного года включительно, не используя циклы. Напишите
+// формулу, использующую только арифметические операторы.
+
+import java.util.Scanner;
 
 public class CoreJavaSolutions {
-    public static int leapYearCount(int Year) {
-        return (year / 4) - (year / 100) + (year / 400);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.println(leapYearCount(x));
     }
-}
-
+    public static int leapYearCount(int year) {
+        return (year/4)-(year/100)+(year/400);
+    }
 /*Реализуйте метод, возвращающий ответ на вопрос: правда ли, что a + b = c?
 Допустимая погрешность – 0.0001 (1E-4).*/
-
-public class CoreJavaSolutions {
     public static boolean doubleExpression(double a, double b, double c) {
         return Math.abs(a + b - c) <= 0.0001;
     }
-}
-
 /*Реализуйте метод, возвращающий true, если среди четырех его аргументов
 ровно два истинны (любые). Составьте формулу с использованием булевых операторов.*/
-
-public class CoreJavaSolutions {
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
         return ((a ^ b) & (c ^ d)) | ((a ^ c) & (b ^ d)) | ((a ^ d) & (b ^ c));
     }
