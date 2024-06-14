e0edc80d97312a294953fcafb3c67dd2bed48de7
Vika_Inozemceva <inozemceva.vika@bk.ru>
2024-06-14 17:07:39 +0300

    The maine method is placed in a separate class for better understanding of the code

--- src/CoreJavaSolutions.java
+++ src/CoreJavaSolutions.java
@@ -1,26 +1,37 @@
-import java.util.Scanner;
-
-//Реализуйте метод, вычисляющий количество високосных лет с начала нашей эры (первого
-// года) до заданного года включительно, не используя циклы. Напишите
-// формулу, использующую только арифметические операторы.
 public class CoreJavaSolutions {
+
+    public static final double EPSILON = 0.0001;
+
+    /**
+     * Реализуйте метод, вычисляющий количество високосных лет с начала нашей эры (первого
+     * года) до заданного года включительно, не используя циклы. Напишите
+     * формулу, использующую только арифметические операторы
+     */
     public static int countLeapYearFromZero(int year) {
-        return (year/4)-(year/100)+(year/400);
+        return (year / 4) - (year / 100) + (year / 400);
     }
-//Реализуйте метод flipBit, изменяющий значение одного бита заданного целого числа на
-// противоположное. Биты должны нумероваться от младшего (индекс 1) к старшему (индекс 32).
-    public static int replacesValueOfANumberBitWithItsOpposite(int value, int bitIndex) {
-        return value^(1<<(bitIndex-1));
+
+    /**
+     * Реализуйте метод flipBit, изменяющий значение одного бита заданного целого числа на
+     * противоположное. Биты должны нумероваться от младшего (индекс 1) к старшему (индекс 32)
+     */
+    public static int flipBit(int value, int bitIndex) {
+        return value ^ (1 << (bitIndex - 1));
     }
-/*Реализуйте метод, возвращающий ответ на вопрос: правда ли, что a + b = c?
-Допустимая погрешность – 0.0001 (1E-4).*/
-    public static boolean differenceOfAllNumbersInModulusIsLessThenValue(double a, double b, double c) {
-        return Math.abs(a + b - c) <= 0.0001;
+
+    /**
+     * Реализуйте метод, возвращающий ответ на вопрос: правда ли, что a + b = c?
+     * Допустимая погрешность – 0.0001 (1E-4)
+     */
+    public static boolean isDifferenceLessThenEpsilon(double termA, double termB, double subC) {
+        return Math.abs(termA + termB - subC) <= EPSILON;
     }
-/*Реализуйте метод, возвращающий true, если среди четырех его аргументов
-ровно два истинны (любые). Составьте формулу с использованием булевых операторов.*/
-    public static boolean trueIfTwoAreTrue(boolean a, boolean b, boolean c, boolean d) {
+
+    /**
+     * Реализуйте метод, возвращающий true, если среди четырех его аргументов
+     *ровно два истинны (любые). Составьте формулу с использованием булевых операторов
+     */
+    public static boolean isTwoOfFour(boolean a, boolean b, boolean c, boolean d) {
         return ((a ^ b) & (c ^ d)) | ((a ^ c) & (b ^ d)) | ((a ^ d) & (b ^ c));
     }
 }
-
